ca4dafbf3aeb1e1ad234fce0aa952b41a276a16a
Juan Casal <juan.5ht@gmail.com>
2026-06-07 17:06:17 -0300

    Add Fischer esterification simulator

---
+++ fischer-esterification-simulator/esters.js
@@ -0,0 +1,26 @@
+export const esters = [
+  {
+    name: "Ethyl acetate",
+    acid: "Acetic acid",
+    alcohol: "Ethanol",
+    Keq: 3.45
+  },
+  {
+    name: "Methyl acetate",
+    acid: "Acetic acid",
+    alcohol: "Methanol",
+    Keq: 5.20
+  },
+  {
+    name: "Butyl acetate",
+    acid: "Acetic acid",
+    alcohol: "1-Butanol",
+    Keq: 4.60
+  },
+  {
+    name: "Isoamyl acetate",
+    acid: "Acetic acid",
+    alcohol: "Isoamyl alcohol",
+    Keq: 5.22
+  }
+];
